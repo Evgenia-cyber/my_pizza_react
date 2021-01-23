8826df92c963f92c73286afcfbda3cc792b7d8d6
Evgenia Kurineva <kurineva85@mail.ru>
2021-01-23 13:56:26 +0300

    26.0: вывела данные из redux на странице корзины. В cartReducer.js  добавила переменную, где отсортированы пиццы с одинаковым id, типом теста и размером теста

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -5,17 +5,46 @@ const initialState = {
   totalCount: 0,
   totalPrice: 0,
   countPizzasInGroup: [], //[{id:5, countPizzas:1},{...},{...}]
+  sortedItems: [],
 };
 
 const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_PIZZA_TO_CART:
+      const sortedItems = [...state.items, action.pizza].reduce(
+        (tmp, value) => {
+          // Находим одинаковые пиццы
+          const match = tmp.find((y) =>
+            y.some(
+              (z) =>
+                z.id === value.id &&
+                z.activeType === value.activeType &&
+                z.activeSize === value.activeSize,
+            ),
+          );
+
+          // Если такой массив есть, добавляем в него новую пиццу
+          if (match) {
+            match.push(value);
+          } else {
+            // Иначе создаем новый массив
+            tmp.push([value]);
+          }
+
+          return tmp;
+        },
+        [],
+      );
       return {
         ...state,
         items: [...state.items, action.pizza],
         totalCount: state.totalCount + 1,
         totalPrice: state.totalPrice + action.pizza.price,
-        countPizzasInGroup:[...state.countPizzasInGroup,action.countPizzasInGroupObject]
+        countPizzasInGroup: [
+          ...state.countPizzasInGroup,
+          action.countPizzasInGroupObject,
+        ],
+        sortedItems: sortedItems,
       };
 
     default:
